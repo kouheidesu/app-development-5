2b9dc1de08d864f1eafad112a18760472006a106
gyarmex <gyarmex@gmail.com>
2025-09-24 06:49:54 +0900

    最新コードに変更

---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+phpinfo();
+$sample = "sampel";
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,15 @@
 <?php
 
+namespace App\Http\Controllers;
+
 use App\Http\Controllers\ThankController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [ThankController::class, 'index']);
 Route::post('/thanks/increment', [ThankController::class, 'increment'])->name('thanks.increment');
+
+Route::get('/debug-test', function () {
+    $n = 123; // ← ここにブレークポイント
+    return $n;
+});
